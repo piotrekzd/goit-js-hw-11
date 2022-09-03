6e811ed7a7c2281772163d47ccc159cb37517db0
Piotr Zdebski <piotrek.zdebski@gmail.com>
2022-09-03 12:47:30 +0200

    .

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import simpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+import Notiflix from 'notiflix';
+import { debounce } from 'lodash';
+import ax
+
+
+const API_KEY = '29677253-331af6074919a5bc21fef774b';
